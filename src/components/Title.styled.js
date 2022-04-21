21d230f490eccdf87def3d186b13cd7bc05a6daa
“Anamika <shivirajput2408p@gmail.com>
2022-04-21 20:22:44 +0530

    Box-Office change ui

--- src/components/Title.styled.js
+++ src/components/Title.styled.js
@@ -3,7 +3,8 @@ import styled from 'styled-components';
 export const TitleWrapper = styled.div`
   text-align: center;
   margin: 0 0 40px;
-  background-image: url('https://upload.wikimedia.org/wikipedia/commons/e/e6/Background_Colors_by_Peak_Hora17.jpg');
+  /* background-image: url('https://upload.wikimedia.org/wikipedia/commons/e/e6/Background_Colors_by_Peak_Hora17.jpg'); */
+  background-color: #A3586D;
 
   h1 {
     color: ${({ theme }) => theme.mainColors.blue};
--- src/components/styled.js
+++ src/components/styled.js
@@ -4,19 +4,21 @@ export const FlexGrid = styled.div`
   display: flex;
   justify-content: center;
   flex-wrap: wrap;
+  background-color:#F2EAED;
 `;
 
 export const SearchCard = styled.div`
   width: 300px;
   height: 100%;
   margin: 0 15px 40px;
+  background-color: #BF988F;
+
   .img-wrapper {
     width: 100%;
     border-radius: 40px;
     height: 420px;
     overflow: hidden;
     border: 1px solid #ddd;
-
     img {
       object-fit: cover;
       height: 100%;
