4006a83b68928d608ead8f6791a4a49da6d195ed
“Anamika <shivirajput2408p@gmail.com>
2022-04-22 10:42:13 +0530

    deploye the app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 
 import Home from './pages/Home';
 import Starred from './pages/Starred';
 import Show from './pages/Show';
+import {HashRouter} from 'react-router-dom'
 
 const theme = {
   mainColors: {
@@ -17,14 +18,14 @@ const theme = {
 function App() {
   return (
     <ThemeProvider theme={theme}>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route exact path='/' element={<Home/>}/>
         <Route exact path='/starred' element={<Starred/>}/>
         <Route exact path='/show/:id' element={<Show/>}/>
         <Route path='*' element="page not fount"/>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
     </ThemeProvider>
   );
 }
